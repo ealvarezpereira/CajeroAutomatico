348665047c6f9a4a62fe63f363c1c769a6cd4a6e
ealvarezpereira@danielcastelao.org <quique@LAPTOP-2PRGJ9F7>
2018-02-26 17:27:45 +0100

    Clase Clientes creada, parametros, constructores, getters, setters, tostring.

--- src/com/quique/cajero/Clientes.java
+++ src/com/quique/cajero/Clientes.java
@@ -11,4 +11,60 @@ package com.quique.cajero;
  */
 public class Clientes {
     
+    
+    private String nombre;
+    private String usuario;
+    private String contraseña;
+    private float saldo;
+
+    public Clientes() {
+    }
+
+    public Clientes(String nombre, String usuario, String contraseña, float saldo) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.saldo = saldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario: " + usuario + ", Contraseña: " + contraseña + ", Nombre: " + nombre + ", Saldo: " + saldo;
+    }
+    
+    
+    
+            
 }
