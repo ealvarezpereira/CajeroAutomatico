629817f787f5a5c0e9bf70ba7b132b7a7c8c2de5
ealvarezpereira@danielcastelao.org <>
2018-02-26 18:05:29 +0100

    Clase Cajero. Añadido metodo para insertar cliente.

--- src/com/quidave/cajero/Cajero.java
+++ src/com/quidave/cajero/Cajero.java
@@ -5,10 +5,67 @@
  */
 package com.quidave.cajero;
 
+import com.quique.cajero.Clientes;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author quique
  */
 public class Cajero {
-    
+
+    File fich;
+    private PrintWriter escribir;
+    private Clientes cliente;
+    private String line;
+
+    public void registrarUsuario() {
+        /**
+         * Estos JOptionPane son para darle valores al constructor de Libro
+         *
+         * @param usu Usuario del cliente
+         * @param ctra Contraseña de su cuenta
+         * @param nom Nombre del cliente
+         * @param saldo Saldo de su cuenta
+         */
+        String usu = JOptionPane.showInputDialog("Usuario");
+        String ctra = JOptionPane.showInputDialog("Contraseña");
+        String nom = JOptionPane.showInputDialog("Nombre");  
+        float saldo = Float.parseFloat(JOptionPane.showInputDialog("Saldo de la cuenta"));
+
+
+        try {
+            /**
+             * Fich pasa a ser un nuevo archivo file
+             *
+             * @param escribir pasa a ser un nuevo PrintWriter que tiene dentro
+             * un FileWriter con el fichero.
+             *
+             * Introducimos el objeto de tipo libro en el fichero.
+             */
+            fich = new File("cajero.txt");
+            escribir = new PrintWriter(new FileWriter(fich, true));
+            escribir.println(cliente = new Clientes(usu, ctra, nom, saldo));
+
+        } catch (FileNotFoundException ex) {
+            /**
+             * Excepcion mas estricta Excepcion general
+             *
+             * @param ex Excepcion de tipo FileNotFoundException
+             * @param ex (Segundo ex) Excepcion de tipo IOException
+             */
+            System.out.println("Error de escritura" + ex);
+
+        } catch (IOException ex) {
+            System.out.println("Error de escritura" + ex);
+        } finally {
+            escribir.close();
+        }
+
+    }
 }
