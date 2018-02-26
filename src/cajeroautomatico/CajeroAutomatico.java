16d4dcbca58048dba2b3f0c5ecb885048b950d55
ealvarezpereira@danielcastelao.org <>
2018-02-26 18:06:00 +0100

    Llamada a display.

--- src/cajeroautomatico/CajeroAutomatico.java
+++ src/cajeroautomatico/CajeroAutomatico.java
@@ -5,6 +5,8 @@
  */
 package cajeroautomatico;
 
+import com.quique.cajero.Display;
+
 /**
  *
  * @author quique
@@ -15,7 +17,12 @@ public class CajeroAutomatico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        Display dis = new Display();
+        
+        dis.setVisible(true);
+        
     }
     
 }
